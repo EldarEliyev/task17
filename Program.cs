bffd858ec5706053521ac6301d8c766b679bda69
eldar23451 <liyev8024@gmail.com>
2024-05-16 21:20:39 +0400

    task file is added

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+//for(int i=0; i<=99; i++)
+//{
+//    Console.WriteLine(i);
+//}
+//for(int i=0; i<=10; i++)        
+//{
+//    if(i==3) continue;
+//    if(i==9) continue;
+//    Console.WriteLine(i);
+//string[] names = { "gunel", "isa mellim", "ferid", "eldar" };
+//for (int i = 0; i < names.Length; i++)
+//{
+//    Console.WriteLine(names[i]);
+//Console.WriteLine("please enter your name");
+//string name=Console.ReadLine();
+//if (name == "admin")
+//{
+//    Console.WriteLine("salam aleykum admin");
+
+//}else if (name != "admin")
+//{
+//    Console.WriteLine("welcome user :"+name);
+
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+
+//string wanterword = "apple";
+//int counter = 5;
+//do
+//{
+//    Console.WriteLine("please enter a word");
+//    string userInput=Console.ReadLine();
+
+//    if (string.IsNullOrEmpty(userInput))
+//    {
+//        Console.ForegroundColor = ConsoleColor.Red;
+//        Console.WriteLine("please enter a word in a correct format");
+//        Console.ForegroundColor= ConsoleColor.Green;
+
+//    }
+//    else
+//    {
+//        if (userInput.ToLower() == wanterword.ToLower())
+//        {
+//            Console.WriteLine($"Congratulations you {wanterword}fond");
+//        }
+//        else
+//        {
+//                Console.WriteLine("wrong answer try again");
+////            counter--;
+//int numberover = 78;
+//int counter = 5;
+//do
+//{
+//    Console.WriteLine("please enter number");
+//    int userInput=Convert.ToInt32(Console.ReadLine());
+//    if (userInput < numberover)
+//    {
+//        Console.WriteLine("Up...");
+//        counter--;
+//    }
+//    else
+//    {
+//        Console.WriteLine($"Congratulations {numberover}you found that number");
+
+
+//    }
+//} while (counter>0);
+            
+    
+
+
